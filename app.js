7950e1c38cda9565e04ebf64aa2653533fccb06e
Szilard Udroiu <uszilard@gmail.com>
2019-11-30 10:17:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+class Employee {
+    constructor() {
+        this.name = name;
+        this.id = id;
+        this.title = title;
+    }
+    getName() {
+        return name
+    }
+    getId() {
+        return id
+    };
+    getEmail() {
+
+    };
+    getRole() { return "Employee" };
+}
+
+module.exports = Employee
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/Intern.test.js
@@ -0,0 +1,27 @@
+const Intern = require("../lib/Intern");
+
+test("Can set school via constructor", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
+
+for (var i = 0; i > 5, i++) {
+  function getIDindex(index) {
+    console.log("line number " + i)
+
+  };
+};
+}
